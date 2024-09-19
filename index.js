071906af454877814369dffe9c26bf5b07bbbe24
RomannaBidnyk <romanna.bidnyk@gmail.com>
2024-09-19 19:55:09 -0400

    5. fixed number of attempts

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ function setup() {
   console.log(`target number: ${targetNumber}`);
 
   // Reset number of attempts
-  maxNumberOfAttempts = 0;
+  maxNumberOfAttempts = 5;
 
   // Enable the input and submit button
   submitButton.disabeld = false;
